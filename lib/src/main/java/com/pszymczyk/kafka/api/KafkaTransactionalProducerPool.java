86fdc297c53d2c058d461f6bd31867174b9b1d32
Pawel Szymczyk <pawel.szymczyk@displate.com>
2023-09-11 14:28:14 +0200

    add autocloseable

--- lib/src/main/java/com/pszymczyk/kafka/api/KafkaTransactionalProducerPool.java
+++ lib/src/main/java/com/pszymczyk/kafka/api/KafkaTransactionalProducerPool.java
@@ -2,7 +2,7 @@ package com.pszymczyk.kafka.api;
 
 import org.apache.kafka.clients.producer.Producer;
 
-public interface KafkaTransactionalProducerPool<K, V> {
+public interface KafkaTransactionalProducerPool<K, V> extends AutoCloseable {
 
     Producer<K,V> getTransactionalProducer();
 }
